0cac724c7241758cc9d2e5697df2aaf36ab4c009
Ho33ein <ho33ein.b@gmail.com>
2018-03-07 13:17:09 +0330

    delete subheader menu

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -310,53 +310,48 @@ $newsVip = $data['getNewsVip'];
 
 
 
-   <script>(function (w, d, s, l, i) {
-           w[l] = w[l] || [];
-           w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-           var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-           j.async = true;
-           j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-           f.parentNode.insertBefore(j, f);
-       })(window, document, 'script', 'dataLayer', 'GTM-52DB6Z');</script><!-- End Google Tag Manager -->
-   <script> var newSearchPopup = true </script>
-   <script src="public/js/js_jquery.js"></script>
-   <script type='text/javascript'>var iDkConfig = new DkConfig();
-       iDkConfig.IsCrawler = false;
-       iDkConfig.TemplateServerUrl = 'https://template.digi-kala.com/digikala/';
-       iDkConfig.WebPushApiUrl = 'https://webpushapi.digikala.com/';
-       iDkConfig.IsDKNet = false;
-       iDkConfig.DigiKalaWebApiUrl = 'https://api.digikala.com/';
-       iDkConfig.ServiceUrl = '';
-       iDkConfig.SearchServiceUrl = 'https://search.digikala.com/';
-       iDkConfig.FileServerUrl = 'https://file.digi-kala.com/digikala/';
-       iDkConfig.TvFileServerUrl = 'https://tv.digikala.com/';
-       iDkConfig.AccountSiteUrl = 'https://accounts.digikala.com/';
-       iDkConfig.DigiKalaMagUrl = '';
-       iDkConfig.IsLogin = 'False';
-       iDkConfig.AutoCompleteUrl = 'https://search.digikala.com/api/AutoComplete/';
-       var ServiceUrl = '';
-       var ClientWebApiServiceUrl = 'https://api.digikala.com/';
-       var SearchServiceUrl = 'https://search.digikala.com/';
-       var FileServerUrl = 'https://file.digi-kala.com/digikala/';
-       var TvFileServerUrl = 'https://tv.digikala.com/';
-       var TemplateServerUrl = 'https://template.digi-kala.com/digikala/';
-       var AccountSiteUrl = 'https://accounts.digikala.com/';</script>
-   <script src="public/js/js_public.js"></script>
-   <script src="public/js/js_xdomainrequest.js"></script>
-   <script src="public/js/js_main.js"></script>
+<script>(function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+        var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-52DB6Z');</script><!-- End Google Tag Manager -->
+<script> var newSearchPopup = true </script>
+<script src="public/js/js_jquery.js"></script>
+<script type='text/javascript'>var iDkConfig = new DkConfig();
+    iDkConfig.IsCrawler = false;
+    iDkConfig.TemplateServerUrl = 'https://template.digi-kala.com/digikala/';
+    iDkConfig.WebPushApiUrl = 'https://webpushapi.digikala.com/';
+    iDkConfig.IsDKNet = false;
+    iDkConfig.DigiKalaWebApiUrl = 'https://api.digikala.com/';
+    iDkConfig.ServiceUrl = '';
+    iDkConfig.SearchServiceUrl = 'https://search.digikala.com/';
+    iDkConfig.FileServerUrl = 'https://file.digi-kala.com/digikala/';
+    iDkConfig.TvFileServerUrl = 'https://tv.digikala.com/';
+    iDkConfig.AccountSiteUrl = 'https://accounts.digikala.com/';
+    iDkConfig.DigiKalaMagUrl = '';
+    iDkConfig.IsLogin = 'False';
+    iDkConfig.AutoCompleteUrl = 'https://search.digikala.com/api/AutoComplete/';
+    var ServiceUrl = '';
+    var ClientWebApiServiceUrl = 'https://api.digikala.com/';
+    var SearchServiceUrl = 'https://search.digikala.com/';
+    var FileServerUrl = 'https://file.digi-kala.com/digikala/';
+    var TvFileServerUrl = 'https://tv.digikala.com/';
+    var TemplateServerUrl = 'https://template.digi-kala.com/digikala/';
+    var AccountSiteUrl = 'https://accounts.digikala.com/';</script>
+<script src="public/js/js_public.js"></script>
+<script src="public/js/js_xdomainrequest.js"></script>
+<script src="public/js/js_main.js"></script>
 
 
 
 
-   <script type='text/javascript' src='public/js/jquery/jquery.js?ver=2.0.5'></script>
+<script type='text/javascript' src='public/js/jquery/jquery.js?ver=2.0.5'></script>
 <script type='text/javascript'
         src='public/js/jquery/jquery-migrate.min.js?ver=2.0.5'></script>
 
-<script src="public/js/js_public.js?v=LWsGw_qSybcpEPGlMdah59AY_VUrUk4Vs5LiLIzcDVc1"></script>
-<script src="public/js/js_xdomainrequest.js?v=Ic2FfCKfX6xdPI1Jw9VidPrd4w4Ij8BW7O9rAhd6b9A1"></script>
-<script src="public/js/js_main.js?v=7TforAN8Kth9M52b0-uWjqite87JW22DKFXR9sHSufs1"></script>
-<script src="public/js/js_pagePath.js?v=Xyyl-H9AlOT8fWAgNY1sD-8bhQv1glWO-0MiC_Wx8rU1"></script>
-<script src="public/js/js_emstrack.js?v=ZoELSPG0SdWVAMBXGZZM03-7uWDmCv9UGrjOM4b7SI41"></script>
 
 
 <script type='text/javascript'

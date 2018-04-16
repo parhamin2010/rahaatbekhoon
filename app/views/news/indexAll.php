894bade05132390a22210559158982493ae45185
Antonio <parhamin2010@gmail.com>
2018-04-16 11:32:06 +0430

    صفحه تاپیک تکمیلش کن

--- app/views/news/indexAll.php
+++ app/views/news/indexAll.php
@@ -232,6 +232,45 @@
     });
 </script>
 
+
+
+
+<script>(function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+        var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-52DB6Z');</script><!-- End Google Tag Manager -->
+<script> var newSearchPopup = true </script>
+<script src="public/js/js_jquery.js"></script>
+<script type='text/javascript'>var iDkConfig = new DkConfig();
+    iDkConfig.IsCrawler = false;
+    iDkConfig.TemplateServerUrl = 'https://template.digi-kala.com/digikala/';
+    iDkConfig.WebPushApiUrl = 'https://webpushapi.digikala.com/';
+    iDkConfig.IsDKNet = false;
+    iDkConfig.DigiKalaWebApiUrl = 'https://api.digikala.com/';
+    iDkConfig.ServiceUrl = '';
+    iDkConfig.SearchServiceUrl = 'https://search.digikala.com/';
+    iDkConfig.FileServerUrl = 'https://file.digi-kala.com/digikala/';
+    iDkConfig.TvFileServerUrl = 'https://tv.digikala.com/';
+    iDkConfig.AccountSiteUrl = 'https://accounts.digikala.com/';
+    iDkConfig.DigiKalaMagUrl = '';
+    iDkConfig.IsLogin = 'False';
+    iDkConfig.AutoCompleteUrl = 'https://search.digikala.com/api/AutoComplete/';
+    var ServiceUrl = '';
+    var ClientWebApiServiceUrl = 'https://api.digikala.com/';
+    var SearchServiceUrl = 'https://search.digikala.com/';
+    var FileServerUrl = 'https://file.digi-kala.com/digikala/';
+    var TvFileServerUrl = 'https://tv.digikala.com/';
+    var TemplateServerUrl = 'https://template.digi-kala.com/digikala/';
+    var AccountSiteUrl = 'https://accounts.digikala.com/';</script>
+<script src="public/js/js_public.js"></script>
+<script src="public/js/js_xdomainrequest.js"></script>
+<script src="public/js/js_main.js"></script>
+
+
 <script type='text/javascript' src='public/js/jquery/jquery.js?ver=2.0.5'></script>
 <script type='text/javascript'
         src='public/js/jquery/jquery-migrate.min.js?ver=2.0.5'></script>
@@ -262,6 +301,7 @@
         src='public/js/video-js-player.min.js?ver=2.0.5'></script>
 
 <script type='text/javascript' src='public/js/media-player.min.js?ver=2.0.5'></script>
+
 
 
 

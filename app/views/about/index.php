9400914c0691bae37d2066be3f66cab3f6c3ae55
Antonio <parhamin2010@gmail.com>
2018-04-18 10:15:02 +0430

    responsive

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -73,11 +73,32 @@ Model::sessionInit();
 
 
 </head>
-<body class="rtl home page-template page-template-page-home page-template-page-home-php page page-id-123 wmax">
-<div class="homepage">
+<body class="rtl home page-template page-template-page-home page-template-page-home-php page page-id-123 wmax"
+      style="font-size: 12pt;">
+
+<!--   <a href="https://raahatbekhar.com"> <img class="animated pulsee infinite" src="public/images/raahatbekhar.png" style="width: 175px;-->
+<!--    height: 109px;-->
+<!--    position: fixed;-->
+<!--    z-index: 200;-->
+<!--    margin-right: 89.5%;-->
+<!--    margin-top: 170px;-->
+<!--    padding: 5px;-->
+<!--    text-align: center;-->
+<!--    border-top-right-radius: 10px;-->
+<!--    border-bottom-right-radius: 10px;-->
+<!--    padding: 0px;-->
+<!--    box-shadow: 0px 0 10px 4px rgba(0,0,0,0.5);-->
+<!--    "/>-->
+<!--   </a>-->
+
 <?php require('app/views/include/login.php'); ?>
-    <!-- ========== NAVBAR ========== -->
-    <?php require('app/views/include/header.php'); ?>
+
+
+<div class="container" style="padding: 0 !important; color: ">
+    <main class="home__page" style="background: rgb(43, 57, 63);">
+        <div class="homepage">
+            <!-- ========== NAVBAR ========== -->
+            <?php require('app/views/include/header.php'); ?>
     <!-- ========== NAVBAR : END ========== -->
     <div id="W-100" style="background: rgb(43, 57, 63);">
         <div class="inner-wraper">
@@ -110,13 +131,15 @@ Model::sessionInit();
                 </section>
             </div>
         </div>
-        <div class="clear"></div>
 
     </div>
-    </div>
+            <?php require('app/views/include/footer.php'); ?>
+
+        </div>
+
+</div>
 
     <!-- ========================= FOOTER ========================= -->
-    <?php require('app/views/include/footer.php'); ?>
     <!-- ========================= FOOTER : END========================= -->
 
     <!-- JavaScripts placed at the end of the document so the pages load faster -->
@@ -142,4 +165,43 @@ Model::sessionInit();
 <script src="public/js/Track-player.js"></script>
 <script src="public/js/wow.min.js"></script>
 <script src="public/js/pace.js"></script>
+
+
+
+
+<script>(function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+        var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-52DB6Z');</script><!-- End Google Tag Manager -->
+<script> var newSearchPopup = true </script>
+<script src="public/js/js_jquery.js"></script>
+<script type='text/javascript'>var iDkConfig = new DkConfig();
+    iDkConfig.IsCrawler = false;
+    iDkConfig.TemplateServerUrl = 'https://template.digi-kala.com/digikala/';
+    iDkConfig.WebPushApiUrl = 'https://webpushapi.digikala.com/';
+    iDkConfig.IsDKNet = false;
+    iDkConfig.DigiKalaWebApiUrl = 'https://api.digikala.com/';
+    iDkConfig.ServiceUrl = '';
+    iDkConfig.SearchServiceUrl = 'https://search.digikala.com/';
+    iDkConfig.FileServerUrl = 'https://file.digi-kala.com/digikala/';
+    iDkConfig.TvFileServerUrl = 'https://tv.digikala.com/';
+    iDkConfig.AccountSiteUrl = 'https://accounts.digikala.com/';
+    iDkConfig.DigiKalaMagUrl = '';
+    iDkConfig.IsLogin = 'False';
+    iDkConfig.AutoCompleteUrl = 'https://search.digikala.com/api/AutoComplete/';
+    var ServiceUrl = '';
+    var ClientWebApiServiceUrl = 'https://api.digikala.com/';
+    var SearchServiceUrl = 'https://search.digikala.com/';
+    var FileServerUrl = 'https://file.digi-kala.com/digikala/';
+    var TvFileServerUrl = 'https://tv.digikala.com/';
+    var TemplateServerUrl = 'https://template.digi-kala.com/digikala/';
+    var AccountSiteUrl = 'https://accounts.digikala.com/';</script>
+<script src="public/js/js_public.js"></script>
+<script src="public/js/js_xdomainrequest.js"></script>
+<script src="public/js/js_main.js"></script>
+
 </html>

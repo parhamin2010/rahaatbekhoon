0da9ed2e955dc944c011ebc5096a370c8b5c0626
Antonio <parhamin2010@gmail.com>
2018-04-18 13:26:10 +0430

    responsive

--- app/views/include/headerMobile.php
+++ app/views/include/headerMobile.php
@@ -7,7 +7,7 @@ $level = Model::sessionGet('level');
     <div class="off-canvas-panel-wrapper_mo">
         <div class="off-canvas-logo">
             <a href="<?= URL; ?>" style="text-align: center">
-                <img style="width: 95px;margin: 25px 0 0 0px; filter: sepia(100%); "
+                <img style="width: 95px;margin: 25px 0 0 0px; filter: brightness(10.25); "
                      src="public/images/pixel-perfect-final-v02-01.png"/>
             </a>
         </div>

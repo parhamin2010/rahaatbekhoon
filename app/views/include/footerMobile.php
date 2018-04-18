cda967c38fb36d4a52ed8b304c88fa89c039c50b
Antonio <parhamin2010@gmail.com>
2018-04-18 14:23:49 +0430

    responsive

--- app/views/include/footerMobile.php
+++ app/views/include/footerMobile.php
@@ -8,9 +8,9 @@
                 <div class='subscribe-bar-menu clearfix'>
 
                     <div class='menu-right'>
-                        <p style="font-size: 16px;color: #b2b9bd;margin-bottom: 18px" class='title'>درباره <?= NAME; ?>
+                        <p style="font-size: 14pt;color: #fff;margin-top: 18px;text-align: center">درباره <?= NAME; ?>
                         </p>
-                        <p style="font-size: 9pt;padding-left: 35px;direction: rtl;text-align: justify">
+                        <p style="font-size: 10pt;padding: 15px;direction: rtl;text-align: justify;color: #e7d080">
                             لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
                             چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی
                             تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در
@@ -21,35 +21,50 @@
                             اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
                         </p></div>
                 </div>
-                <div class='subscribe-bar-form'>
-                    <span class='title' style="color: #b2b9bd">ارتباط با ما</span>
+                <div style="text-align: center;font-size: 16pt">
+                    <span style="color: #fff;">ارتباط با ما</span>
                     <div class='menu-right' style="width: 100%">
-                        <div  style="width:50%;float: right;"><i class="icon icon-info-message" style="margin-top: 5px;float: right"></i><p style=";display:inline;color:#b2b9bd;font-size: 13px;width: 100%">&nbsp;ایمیل :</p></div><div style="width:50%;float: left;text-align: left"><p style="display:inline;font-size: 11pt">&nbsp&nbsp&nbspinfo @ irandall.ir</p></div><br>
-                        <div  style="width:50%;float: right"><i class="icon icon-info-tell" style="margin-top: 5px;float: right"></i><p style="display:inline;color:#b2b9bd;font-size: 13px;">&nbsp;تلفن :</p></div><div style="width:50%;float: left;text-align: left"><p style="direction: ltr;display:inline;font-size: 12pt ">051&nbsp33123313</p></div>
-                        <div  style="width:50%;float: right"><i class="icon icon-info-tell" style="margin-top: 5px;float: right"></i><p style="display:inline;color:#b2b9bd;font-size: 13px;">&nbsp;فکس :</p></div><div style="width:50%;float: left;text-align: left"><p style="direction: ltr;display:inline;font-size: 12pt ">&nbsp051&nbsp33123314</p></div>
-                    </div><br/><br/><br/><br/>
+                        <div style="direction: rtl;width:50%;float: right;"><i class="icon icon-info-message"
+                                                                style="margin-top: 5px;float: right"></i>
+                            <p style=";display:inline;color:#b2b9bd;font-size: 15px;width: 100%">&nbsp;ایمیل :</p></div>
+                        <div style="width:50%;float: left;text-align: center"><p style=";color: #e7d080;display:inline;font-size: 11pt">
+                                &nbsp&nbsp&nbspinfo @ irandall.ir</p></div>
+                        <br>
+                        <div style="direction: rtl;width:50%;float: right"><i class="icon icon-info-tell"
+                                                               style="margin-top: 5px;float: right"></i>
+                            <p style="display:inline;color:#b2b9bd;font-size: 15px;">&nbsp;تلفن :</p></div>
+                        <div style="width:50%;float: left;text-align: center"><p
+                                style="direction: ltr;display:inline;font-size: 12pt;color: #e7d080 ">051&nbsp33123313</p></div>
+                        <div style="direction: rtl;width:50%;float: right"><i class="icon icon-info-tell"
+                                                               style="margin-top: 5px;float: right"></i>
+                            <p style="display:inline;color:#b2b9bd;font-size: 15px;">&nbsp;فکس :</p></div>
+                        <div style="direction: rtl;width:50%;float: left;text-align: center"><p
+                                style="direction: ltr;display:inline;font-size: 12pt ;color: #e7d080">&nbsp051&nbsp33123314</p></div>
+                    </div>
+                    <br/><br/><br/><br/>
 
                     <div class='social-panel clearfix'>
                         <hr>
-                        <p style="float:right;padding-top: 12px;color: #b2b9bd">شبکه های اجتماعی ما :</p>
-                        <ul class='right social-linkbox' style="float: left">
-                            <li><a target='_blank' title='فیس بوک'
-                                   href='https://www.facebook.com/irandall'><i
-                                        class='icon icon-footer-facebook'></i></a></li>
-                            <li><a target='_blank' title='توییتر'
+                        <p style="padding-top: 12px;color: #b2b9bd">شبکه های اجتماعی ما</p><br>
+                        <ul class='right social-linkbox' style="text-align: center;margin-top: -25px;padding-right: 115px">
+                            <li style="float: right;margin: 5px"><a target='_blank' title='فیس بوک'
+                                   href='https://www.facebook.com/irandall'>
+                                    <i
+                                        class='fa fa-facebook-official' style="font-size: 25pt;color: ghostwhite"></i></a></li>
+                            <li style="float: right;margin: 5px"><a target='_blank' title='توییتر'
                                    href='https://www.twitter.com/irandall'><i
-                                        class='icon icon-footer-twitter'></i></a></li>
-                            <li><a target='_blank' title='گوگل پلاس'
+                                        class='fa fa-twitter-square' style="font-size: 25pt;color: ghostwhite"></i></a></li>
+                            <li style="float: right;margin: 5px"><a target='_blank' title='گوگل پلاس'
                                    href='https://plus.google.com/107060095312678131486/posts'><i
-                                        class='icon icon-footer-googleplus'></i></a></li>
-                            <li><a target='_blank' title='اینستاگرام'
+                                        class='fa fa-google-plus-official' style="font-size: 25pt;color: ghostwhite"></i></a></li>
+                            <li style="float: right;margin: 5px"><a target='_blank' title='اینستاگرام'
                                    href='https://www.instagram.com/irandall'><i
-                                        class='icon icon-footer-instagram'></i></a></li>
-                            <li><a target='_blank' title='آپارات'
+                                        class='fa fa-instagram' style="font-size: 25pt;color: ghostwhite"></i></a></li>
+                            <li style="float: right;margin: 5px"><a target='_blank' title='آپارات'
                                    href='www.aparat.com/irandall'><i
-                                        class='icon icon-footer-aparat'></i></a></li>
-                            <li><a target='_blank' title='تلگرام'
-                                   href='www.t.me/irandall'><i class='icon icon-footer-telegram'></i></a>
+                                        class='icon icon-footer-aparat' style="font-size: 25pt;color: ghostwhite"></i></a></li>
+                            <li style="float: right;margin-top: 8px;margin-right: -8px"><a target='_blank' title='تلگرام'
+                                   href='www.t.me/irandall'><i class='fa fa-telegram' style="font-size: 20pt;color: ghostwhite"></i></a>
                             </li>
                         </ul>
                     </div>
@@ -62,9 +77,11 @@
         <div class='wrapper'>
             <div class='inner-wraper clearfix'>
                 <div class='copyright-bar-slogan' style="padding: 0 5px;">
-                    <span id='copyright-full' style="margin: 25px 0;color: #fff;">استفاده از مطالب <?= NAME; ?> فقط برای مقاصد غیر تجاری و با ذکر منبع بلامانع است. کليه حقوق اين سايت متعلق به <?= NAME; ?> می‌باشد.</span>
+                    <span id='copyright-full' style="margin: 25px 0;color: #fff;">استفاده از مطالب <?= NAME; ?> فقط برای مقاصد غیر تجاری و با ذکر منبع بلامانع است. کليه حقوق اين سايت متعلق به <?= NAME; ?>
+                        می‌باشد.</span>
 
-                    <span style="padding: 18px 0;float: left;color: #fff;">Copyright © 2018 <?= str_replace("http://","www.",URL_FOOTER); ?></span>
+                    <span
+                        style="padding: 18px 0;float: left;color: #fff;">Copyright © 2018 <?= str_replace("http://", "www.", URL_FOOTER); ?></span>
                 </div>
 
             </div>

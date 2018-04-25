54206048abaef357421f4dd4c0d86e6bedca27e1
Ho33ein <ho33ein.b@gmail.com>
2018-04-25 14:08:32 +0430

    fix category page loading

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
-require_once('core/raahatbekhon.php');
-require_once('core/controller.php');
-require_once('core/model.php');
-require_once('core/config.php');
-require_once('public/library/google/autoload.php');
-require_once('public/library/PHPMailer/class.phpmailer.php');
-require_once('public/library/PHPMailer/PHPMailerAutoload.php');
-require_once('public/library/mp3trim/phpmp3.php');
-require_once('public/library/MobileDetect/Mobile_Detect.php');
-require_once('public/library/rss/rss_feed.php');
-
-new RaahatBekhon;
+    ignore_user_abort(1); // run script in background
+    set_time_limit(0); // run script forever
+    ini_set("log_errors", 0);
+    error_reporting(E_ERROR | E_PARSE);
+    require_once('core/raahatbekhon.php');
+    require_once('core/controller.php');
+    require_once('core/model.php');
+    require_once('core/config.php');
+    require_once('public/library/google/autoload.php');
+    require_once('public/library/PHPMailer/class.phpmailer.php');
+    require_once('public/library/PHPMailer/PHPMailerAutoload.php');
+    require_once('public/library/mp3trim/phpmp3.php');
+    require_once('public/library/MobileDetect/Mobile_Detect.php');
+    require_once('public/library/rss/rss_feed.php');
+    
+    new RaahatBekhon;
 ?>
